35b6b13d76fdbc4559f29975c45616ae25fd465e
Angella0 <angellasimbwa@gmail.com>
2021-07-30 19:56:43 +0300

    changes

--- student/forms.py
+++ student/forms.py
@@ -1,8 +1,30 @@
 from django import forms
-from django.forms import fields
+from django.forms import ModelForm, widgets
 from .models import Student
 
 class StudentRegistrationForm(forms.ModelForm):
+    
     class Meta:
         model = Student
         fields = "__all__"
+        widgets ={
+          'first_name' : forms.TextInput(attrs={'class':'form-control'}),
+          'last_name' : forms.TextInput(attrs={'class':'form-control'}),
+          'Roll_number' : forms.NumberInput(attrs={'class':'form-control'}),
+          'Student_id' : forms.TextInput(attrs={'class':'form-control'}),
+          'Course_name' : forms.TextInput(attrs={'class':'form-control'}),
+          'country' : forms.TextInput(attrs={'class':'form-control'}),
+          'age' : forms.NumberInput(attrs={'class':'form-control'}),
+          'date_of_birth' : forms.DateInput(attrs={'class':'form-control'}),
+          'Email_address' : forms.TextInput(attrs={'class':'form-control'}),
+          'Date_of_enrollment' : forms.DateInput(attrs={'class':'form-control'}),
+          'Medical_report' : forms.DateInput(attrs={'class':'form-control'}),
+          'languages' : forms.TextInput(attrs={'class':'form-control'}),
+          'gender' : forms.TextInput(attrs={'class':'form-control'}),
+
+          
+          
+          
+          
+          
+        }
--- student/models.py
+++ student/models.py
@@ -5,26 +5,29 @@ from django.db import models
 class Student(models.Model):
     first_name = models.CharField(max_length=12)
     last_name = models.CharField(max_length=12)
+    Roll_number=models.CharField(max_length=15)
+    Student_id=models.CharField(max_length=15)
+    Course_name=models.CharField(max_length=12)
+    Country=models.CharField(max_length=12)
     age = models.PositiveSmallIntegerField()
     date_of_birth = models.DateField()
     Email_address = models.EmailField()
-    Student_id=models.CharField(max_length=15)
     Date_of_enrollment=models.DateField()
-    County=models.CharField(max_length=12)
     Medical_report=models.FileField()
-    Course_name=models.CharField(max_length=12)
-    Roll_number=models.CharField(max_length=15)
+
+
     GENDER_CHOICES=(
         (u'M',u'Male'),
         (u'F',u'Feamle'),
         (u'O',u'Other')
     )
+    gender=models.CharField(max_length=6,choices=GENDER_CHOICES)
+
     LANGUAGE_CHOICES = (
         (u'L', u'Luganda'),
         (u'E', u'English'),
         (u'K', u'Kiswahili')
     )
-    languages = models.CharField(max_length=6, choices=LANGUAGE_CHOICES)
-    gender=models.CharField(max_length=1,choices=GENDER_CHOICES)
+    languages  = models.CharField(max_length=6, choices=LANGUAGE_CHOICES)
 
 
